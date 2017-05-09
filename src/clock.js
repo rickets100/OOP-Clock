0f9971c68c431dcccfa49070381eb774b3b6f0f7
Kay Krewson <kay.k@comcast.net>
2017-05-09 13:56:31 -0700

    initial structure plus getters and setters for hours, minutes and seconds

--- src/clock.js
+++ src/clock.js
@@ -1,1 +1,45 @@
-// your clock code here
+// ===== CLOCK =====
+class Clock {
+  constructor(time) {
+    if (time) {
+      if (time.hours <= 24 && time.hours >= 0) {
+        this.hours = time.hours
+      }
+      if (time.minutes <= 60 && time.minutes >= 0) {
+        this.minutes = time.minutes
+      }
+      if (time.seconds <= 60 && time.seconds >= 0) {
+        this.seconds = time.seconds
+      }
+    }
+    else {
+      this.hours = 0
+      this.minutes = 0
+      this.seconds = 0
+    }
+  }
+
+  getHours() {
+    return this.hours
+  }
+
+  setHours(hours) {
+    this.hours = hours
+  }
+
+  getMinutes() {
+    return this.minutes
+  }
+
+  setMinutes(minutes) {
+    this.minutes = minutes
+  }
+
+  getSeconds() {
+    return this.seconds
+  }
+
+  setSeconds(seconds) {
+    this.seconds = seconds
+  }
+}
--- test/clock.test.js
+++ test/clock.test.js
@@ -1,1 +1,52 @@
-// your test code here
+const chai = require('chai')
+const assert = chai.assert
+const expect = chai.expect
+const myFunction = require('../src/clock')
+
+// Write your tests below
+describe('#main function', function() {
+  // With no inputs, myFunction returns 0
+  // myFunction() --> 0
+  it('function returns 0 with no inputs', function() {
+    var actual = myFunction()
+    var expected = 0
+    assert.equal(expected, actual)
+  })
+  // With a single input, myFunction returns just that input
+  // myFunction(a) --> a
+  // it('returns a single input', function() {
+  //   var actual = myFunction('dog')
+  //   var expected = 'dog'
+  //   assert.equal(expected, actual)
+  // })
+  // With two number inputs, myFunction returns those inputs added together
+  // myFunction(1,2) --> 3
+  // it('returns the sum of two numbers if two numbers are input', function() {
+  //   var actual = myFunction(1, 5)
+  //   var expected = 6
+  //   assert.equal(expected, actual)
+  // })
+  // With two string inputs, myFunction returns the strings concatenated
+  // myFunction('Pika', 'chu') --> 'Pikachu'
+  // it('returns a concatenated string when two strings are input', function() {
+  //   var actual = myFunction('Holy', 'Moly')
+  //   var expected = 'HolyMoly'
+  //   assert.equal(expected, actual)
+  // })
+  // Given any number of string inputs, returns all inputs concatenated
+  // myFunction('This ', 'is ', 'a ', 'string!') --> 'This is a string!'
+  // myFunction('I ', 'like ', 'turtles!') --> 'I like turtles!'
+  // it('returns all inputs concatenated', function() {
+  //   var actual = myFunction('Holy', 'Moly', 'Yikes', 'Etc')
+  //   var expected = 'HolyMolyYikesEtc'
+  //   assert.equal(expected, actual)
+  // })
+  // Given any number of number inputs, returns all inputs summed.
+  // myFunction(1,2,3,4,5,6,7,100) --> 128
+  // myFunction(1,2,3) --> 6
+  // it('returns all numbers concatenated', function() {
+  //   var actual = myFunction(1,2,3,4,5,100)
+  //   var expected = 115
+  //   assert.equal(expected, actual)
+  // })
+})

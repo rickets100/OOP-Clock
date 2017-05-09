b6e90d39c7f1a0933f21926183169a9cf2a91f99
Kay Krewson <kay.k@comcast.net>
2017-05-09 16:46:57 -0700

    most stuff written'

--- src/clock.js
+++ src/clock.js
@@ -2,13 +2,13 @@
 class Clock {
   constructor(time) {
     if (time) {
-      if (time.hours <= 24 && time.hours >= 0) {
+      if (time.hours < 24 && time.hours >= 0) {
         this.hours = time.hours
       }
-      if (time.minutes <= 60 && time.minutes >= 0) {
+      if (time.minutes < 60 && time.minutes >= 0) {
         this.minutes = time.minutes
       }
-      if (time.seconds <= 60 && time.seconds >= 0) {
+      if (time.seconds < 60 && time.seconds >= 0) {
         this.seconds = time.seconds
       }
     }
@@ -17,13 +17,16 @@ class Clock {
       this.minutes = 0
       this.seconds = 0
     }
+    this.isTwentyFour = true
   }
+
 
   getHours() {
     return this.hours
   }
 
   setHours(hours) {
+    if (hours < 24 && hours >= 0)
     this.hours = hours
   }
 
@@ -32,7 +35,9 @@ class Clock {
   }
 
   setMinutes(minutes) {
-    this.minutes = minutes
+    if (minutes < 60 && minutes >= 0) {
+      this.minutes = minutes
+    }
   }
 
   getSeconds() {
@@ -40,6 +45,88 @@ class Clock {
   }
 
   setSeconds(seconds) {
-    this.seconds = seconds
+    if (seconds < 60 && seconds >= 0) {
+      this.seconds = seconds
+    }
+  }
+
+  tick() {
+    this.seconds = this.seconds + 1
+    if (this.seconds == 60) {
+      this.seconds = 0
+      this.minutes = this.minutes + 1
+      if (this.minutes == 60) {
+        this.minutes = 0
+        this.hours = this.hours + 1
+        if (this.hours == 24) {
+          this.hours = 0
+        }
+      }
+    }
+  }
+
+    getFormat() {
+      if (this.isTwentyFour) {
+        return '24-hour format'
+      }
+      else {
+        return '12-hour format'
+      }
+    }
+
+    toggleFormat() {
+      this.isTwentyFour = !(this.isTwentyFour)
+    }
+
+    getTime() {
+      let displayHours = this.getHours()
+      let displayMinutes = this.getMinutes()
+      let displaySeconds = this.getSeconds()
+      let suffix = ''
+
+      if (getFormat() == '12-hour format') {
+        if (displayHours == 12) {
+          suffix = 'PM'
+        }
+        if (displayHours > 12) {
+          displayHours = displayHours - 12
+          suffix = 'PM'
+        }
+        else {
+          suffix = 'AM'
+        }
+      } else {
+        if (displayHours => 12) {
+          suffix = 'PM'
+      } else {
+        suffix = 'AM'
+      }
+
+      // now format as strings
+      if (displayHours < 10) {
+        displayHours = `0${displayHours}:`
+      } else {
+        displayHours = `${displayHours}:`
+      }
+
+      if (displayMinutes < 10) {
+        displayMinutes = `0${displayMinutes}:`
+      } else {
+        displayMinutes = `${displayMinutes}:`
+      }
+
+      if (displaySeconds < 10) {
+        displaySeconds = `0${displaySeconds} `
+      } else {
+        displaySeconds = `0${displaySeconds} `
+      }
+
+      let time = displayHours + displayMinutes + displaySeconds + suffix
+      console.log('time is ', time)
+      return time
+    }
   }
 }
+
+var clock = new Clock({ hours: 16, minutes: 30, seconds: 15 })
+console.log('oh god ', clock)
